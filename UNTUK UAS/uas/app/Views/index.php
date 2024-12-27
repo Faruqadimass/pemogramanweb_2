9b0f5ad93467f79bdf906e9800efdeaa49323f76
FARUQ ADIMAS <faruqadimass@gmail.com>
2024-12-27 20:10:18 +0700

    Add files via upload

---
+++ UNTUK UAS/uas/app/Views/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url() ?>css/style.css" />
+  </head>
+  <body>
+  <header class="container-mb">
+    <h1>Pahe</h1>
+        <a href="<?= base_url('login')?>" class="btn btn-outline-primary">Login</a>
+    </header>
+
+    <main>
+            <div class="konten">
+                <div class="profil">
+                    <h1><strong>Welcome To Genshin WIKI</strong></h1>
+                    <p>Sebagai salah satu dari Sebelas Fatui Harbinger , Arlecchino sangat menghormati Tsaritsa ,
+                        meskipun
+                        dia menyatakan bahwa dia bersedia mengkhianati Tsaritsa jika kepentingan mereka berbeda.
+                        Arlecchino bekerja untuk mencapai tujuannya dengan memperoleh Gnosis atas namanya. Dia menangani
+                        masalah Fatui dengan sangat penting dan tampil anggun dan ramah.</p>
+                    <p>Arlecchino tidak disukai oleh sesama Harbinger, yang percaya bahwa dia kasar dan gila di balik
+                        sikap
+                        tabahnya. Hal ini tampaknya berasal dari cerita bias tentang masa lalu Arlecchino yang
+                        disebarkan oleh Pulcinella , yang tidak disukai Arlecchino. Arlecchino membiarkan rumor
+                        tentang dirinya terus berlanjut, karena dia percaya bahwa tipu daya adalah alat yang berguna.
+                    </p>
+                    <p>Meskipun mengaku sebagai orang Fontainian, Arlecchino sebenarnya berasal dari Khaenri'ah .
+                        Arlecchino
+                        tidak tahu banyak tentang asal usulnya, dia hanya tahu bahwa dia memiliki semacam api terkutuk
+                        yang
+                        mengalir dalam nadinya sejak usia muda. Yang dia tahu hanyalah bahwa dia memiliki hubungan
+                        dengan
+                        Pierro saat dia bergabung dengan Harbingers.</p>
+                </div>
+                <div class="konten-img">
+                        <img class="img1" src="<?= base_url() ?>img/Arlecchino_Wish.webp" alt="Arlecchino_Wish">
+                    </div>
+
+        
+
+    </main>
+    <footer>
+        <p>Web submission <strong>IDCamp 2024</strong> NOVA WIJAYA</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ UNTUK UAS/uas/app/Views/login.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url() ?>css/style.css" />
+  </head>
+  <body>
+  <header class="container-mb">
+    <h1>Pahe</h1>
+        <a href="" class="btn btn-outline-primary">Login</a>
+    </header>
+
+
+        
+
+    </main>
+    <footer>
+        <p>Web submission <strong>IDCamp 2024</strong> NOVA WIJAYA</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
